a0b94d264c66bf463866d0141266d71993ea3886
alvin phiri <aphiri1658@gmail.com>
2024-12-09 17:43:54 +0200

    H

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS healthcare;
 CREATE DATABASE healthcare;
 
 
---
+++ run_sql.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\xampp\mysql\bin\mysql --defaults-file=C:\Users\dell\.my.cnf healthcare < mysql.sql
